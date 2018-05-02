0cb9dd7577a98ee8722aec54828f89b95a62be59
pra-kri <37740552+pra-kri@users.noreply.github.com>
2018-05-02 19:32:49 +0100

    Update Optimisation - RandomSearch.py

--- Optimisation - RandomSearch.py
+++ Optimisation - RandomSearch.py
@@ -1,15 +1,19 @@
+
 # implement the random search for finding the minima of a function
 
 import random
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 # will attempt to minimse a 3d fucntion wrt F(x,y)
 # Himmeblau's function:
 # F(x,y) = (x^2 + y - 11)^2 + (x + y^2 - 7)^2
-# One local maxima: F = 181.617, at x=-0.270845, y=-0.923039
+# 
 # 4 Local minima, all at F = 0.0
 #    f(3.0, 2.0), f(-2.805118, 3.131312), f(-3.779310, -3.283186), f(3.584428, -1.848126)
-
+#
+# 1 local maxima: F = 181.617, at x=-0.270845, y=-0.923039, but will ignore this, since the aim is to minimise.
 
 
 def function_to_minimise(x, y):
@@ -17,8 +21,7 @@ def function_to_minimise(x, y):
     return z 
     
 
-# search space for x = {-5, 5}, best for Himmelblau's Function
-
+# search space for x = -5 to 5.
 
 def random_search(in_function, space_start = -5, space_end=5):
 
@@ -36,10 +39,29 @@ def random_search(in_function, space_start = -5, space_end=5):
          
     
     return best
-    
-    
-    
-print(random_search(function_to_minimise))
+
+x = np.linspace(-6, 6, 1000)
+y = np.linspace(-6, 6, 1000)
+X, Y = np.meshgrid(x, y)
+Z = function_to_minimise(X,Y)
+
+
+best_temp = random_search(function_to_minimise)
+
+plt.contour(X, Y, Z, 50, cmap = 'gray')
+plt.plot(best_temp[1], best_temp[2], 'g<')
+print(best_temp)
+
+list_of_bests = []
+list_of_bests.append(best_temp)
+
+
+for i in range(0,20):
+    best_temp1 = random_search(function_to_minimise)
+    plt.plot(best_temp1[1], best_temp1[2], 'r<')
+    list_of_bests.append(best_temp1)
+# !!!!! ALL 4 of the MINIMA get disovered. Cool but not very computationally efficient...wastes a LOT of calculations, and is already quite slow..
+
 
 """
 Notes: 
